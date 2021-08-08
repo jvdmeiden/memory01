4db265ea6cb39a12ea10d5e586d0065bfb6e93d6
Jan van der Meiden <jvdmeiden@gmail.com>
2021-08-08 19:23:27 +0200

    001

---
+++ scripts/memory.js
@@ -0,0 +1,17 @@
+/*
+Author:     Jan van der Meiden
+            jvdmeiden@gmail.com
+Version:    20210806.00  
+Copyright (c) 2021 Jan van der Meiden.
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided this copyright
+notice is preserved. 
+*/
+
+function init() {
+  
+  // const urlParams = new URLSearchParams(window.location.search);
+  // let cardWidth = parseInt(urlParams.get('width'));
+  // console.log(cardWidth);
+  // let cardGap = parseInt(urlParams.get('gap'));
+}
