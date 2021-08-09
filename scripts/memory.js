1239d8b532cb3d5aaea657d3b0a279c0cdd6f108
Jan van der Meiden <jvdmeiden@gmail.com>
2021-08-09 05:33:43 +0200

    opbouw svg

--- scripts/memory.js
+++ scripts/memory.js
@@ -9,23 +9,83 @@ notice is preserved.
 */
 
 var state = [];
-var images = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59'];
+var images = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59'];
+var landscape;
+var fNum = 6;
+var fSize
 
 function init() {
-  var ratio = window.devicePixelRatio || 1;
-  var maxX=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var maxY=window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
-  // var state = [];
-  for(var i=0; i<8; i++) {
+  window.maxX=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  window.maxY=window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+  window.svgObject=document.getElementById("svg1");
+  var fieldSize=Math.max(maxX,maxY);  
+  if ( fieldSize == maxX) {
+    landscape = true;
+  } else {
+    landscape = false;
+  }
+  if ( landscape == true ) {
+    fSize = (maxY - 5 * (fNum + 1))/fNum;
+    var xDelta = (maxX - (fNum*(fSize+5)+5))/2;
+    var yDelta = 0;
+  } else {
+    fSize = (maxX - 5 * (fNum + 1))/fNum;
+    var yDelta = (maxY - (fNum*(fSize+5)+5))/2;
+    var xDelta = 0;
+  }
+    
+  for(var i=0; i<fNum; i++) {
     state[i] = [];
-    for(var j=0; j<8; j++) {
+    for(var j=0; j<fNum; j++) {
         state[i][j] = undefined;
+        newRect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        newRect.setAttribute("x",(xDelta+i*(fSize+5))+5);
+        newRect.setAttribute("y",(yDelta+j*(fSize+5))+5);
+        newRect.setAttribute("width",fSize);
+        newRect.setAttribute("height",fSize);
+        newRect.setAttribute("color","grey");
+        newRect.setAttribute("id",i+"-"+j);
+        newRect.setAttribute("onclick","onClick(evt)");
+        svgObject.appendChild(newRect);
     }
   }
+  var cardCount = (fNum * fNum) / 2;
+  while (cardCount > 0) {
+     var newCard = false;
+     while ( newCard == false ){
+       var curCardNum = Math.floor(Math.random() * images.length);
+       if ( images[ curCardNum ] != "gone" ){ 
+          console.log( images[ curCardNum ]);
+          images[ curCardNum ] = "gone";
+          newCard = "true";
+       }
+     }
+     var laidOne = "false";
+     while ( laidOne == "false" ) {
+        var xPos1 = Math.floor(Math.random() * fNum);
+        var yPos1 = Math.floor(Math.random() * fNum);
+        if ( state[xPos1][yPos1] == undefined ) {
+          state[xPos1][yPos1]={ num: curCardNum , state: "down" };
+          laidOne = "true";
+        }
+     }
+     var laidTwo = "false";
+     while ( laidTwo == "false" ) {
+        var xPos2 = Math.floor(Math.random() * fNum);
+        var yPos2 = Math.floor(Math.random() * fNum);
+        if ( state[xPos2][yPos2] == undefined ) {
+          state[xPos2][yPos2]={ num: curCardNum , state: "down" };
+          laidTwo = "true";
+        }
+     }
+     cardCount -= 1;
+  }
+  // Math.floor(Math.random() * 11);
+  // images.length;
 
-  // const urlParams = new URLSearchParams(window.location.search);
-  // let cardWidth = parseInt(urlParams.get('width'));
-  console.log("X="+maxX+"   Y="+maxY);
-  console.log(state[1][1]);
-  // let cardGap = parseInt(urlParams.get('gap'));
+
+}
+
+function onClick(evt){
+  console.log("hopla");
 }
